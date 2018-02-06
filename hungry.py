7996d2affa7297671f38261a55a5dbc381f801cb
Samuel Aung <developer@gitstudy.com>
2018-02-06 16:41:39 +0800

    added burger

--- hungry.py
+++ hungry.py
@@ -4,6 +4,7 @@ if hungry=="yes":
     print("eat bean palata")
     print("eat pau si")
     print("eat mon si kyaw")
+    print("eat burger")
 else:
     thirsty=raw_input("are you thirsty? ")
     if thirsty=="yes":
